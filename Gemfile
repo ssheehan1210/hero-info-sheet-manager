82d76cebb8372124b2ce6bf44bc64fb9aa9b8c5a
Sam Sheehan <ssheehan1210@yahoo.com>
2017-11-17 10:31:25 -0600

    Getting rid of the multiple copies of files and directories born from the messed up heroku push

--- Gemfile
+++
@@ -1,9 +0,0 @@
-source 'https://rubygems.org'
-
-gem 'sinatra'
-gem 'json'
-gem 'pg'
-gem 'sinatra-activerecord'
-gem 'pry'
-gem 'bcrypt'
-gem "sinatra-cross_origin", "~> 0.3.1"
--- models/HeroModel.rb
+++
@@ -1,7 +0,0 @@
-class Hero < ActiveRecord::Base
-	## don't always have to do this part
-	## but if activer record can't find your table, then this will help
-	self.table_name = 'heroes'
-
-	belongs_to :user
-end
