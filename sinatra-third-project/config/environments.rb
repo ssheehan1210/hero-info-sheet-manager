954393bf0a611846c80f046139780de6e6f16a9c
Sam Sheehan <ssheehan1210@yahoo.com>
2017-11-17 10:09:27 -0600

    Fixing the connection within the environments file

--- sinatra-third-project/config/environments.rb
+++ sinatra-third-project/config/environments.rb
@@ -1,7 +1,7 @@
 configure :production, :development do
 	db = URI.parse(ENV['DATABASE_URL'] || 'postgres://localhost/gathirdproject')
 
-	ActiveRecord::Base.establish.connection(
+	ActiveRecord::Base.establish_connection(
 		:adapter  => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
 		:host     => db.host,
 		:username => db.user,
